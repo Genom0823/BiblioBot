05844b3a6983d6b8ab9fb58db7b7c803100f4117
Genom0823 <58923147+Genom0823@users.noreply.github.com>
2023-04-01 00:28:38 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ import cogs
 
 url = 'https://hon-hikidashi.jp/tag/line-up/'
 
-top_article = cogs.url.get_elements_by_class(url, 'detail__ttl')[0].get('href')
+top_article = cogs.url.get_elements_by_class(url, 'detail__ttl')[0].contents[0].get('href')
 
 print(top_article)
 
